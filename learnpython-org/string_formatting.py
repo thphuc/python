8231842c3a7cb9da467f65962709c1b77470c20e
Phuc Tran <huyphuc0906@gmail.com>
2020-04-11 16:29:44 +0700

    Still can not understand how tuple works. Come back later!

--- learnpython-org/string_formatting.py
+++ learnpython-org/string_formatting.py
@@ -9,7 +9,8 @@ print('%s' % numbers)
 
 print('%.2f' % age)
 
-data = ('Phuc', 'Tran', 32.00)
-formatString = 'Hello %s %s. Your balance is $'
+# data = ('Phuc', 'Tran', 32.00)
+# formatString = 'Hello %s %s. Your balance is $.2f' % (data()[0], data()[1], data()[2])
+# print(formatString)
 
 
