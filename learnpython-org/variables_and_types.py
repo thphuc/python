f2b2266933d1cc276b22bef5d348aa426020ae3e
Phuc Tran <huyphuc0906@gmail.com>
2020-04-11 15:32:49 +0700

    Rename

---
+++ learnpython-org/variables_and_types.py
@@ -0,0 +1,18 @@
+string = 'Hello Python'
+print(string)
+
+string = "Don't worry about that"
+print(string)
+
+# Exercise
+myString = 'hello'
+myFloat = 10.0
+myInt = 20
+
+# testing code
+if myString == "hello":
+    print("String: %s" % myString)
+if isinstance(myFloat, float) and myFloat == 10.0:
+    print("Float: %f" % myFloat)
+if isinstance(myInt, int) and myInt == 20:
+    print("Integer: %d" % myInt)
