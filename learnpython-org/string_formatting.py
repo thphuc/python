022540492d4d1935c91278cdb9875297d470e527
Phuc Tran <huyphuc0906@gmail.com>
2020-04-11 16:14:41 +0700

    String formatting

---
+++ learnpython-org/string_formatting.py
@@ -0,0 +1,15 @@
+name = 'Phuc'
+print('Hello %s!' % name)
+
+age = 32
+print('%s is %d years old.' % (name, age))
+
+numbers = [1, 2, 3]
+print('%s' % numbers)
+
+print('%.2f' % age)
+
+data = ('Phuc', 'Tran', 32.00)
+formatString = 'Hello %s %s. Your balance is $'
+
+
